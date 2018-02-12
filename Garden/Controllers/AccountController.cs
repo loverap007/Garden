48db28f2434c45c4f36fb67aefcb05cc44185e97
Den Borodachenko <loverap007@gmail.com>
2018-02-12 19:25:28 +0300

    Added e-mail confirmation after registration

--- Garden/Controllers/AccountController.cs
+++ Garden/Controllers/AccountController.cs
@@ -59,8 +59,15 @@ namespace Garden.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                var user = await _userManager.FindByNameAsync(model.Email);
+                if (user != null)
+                {
+                    if (!await _userManager.IsEmailConfirmedAsync(user))
+                    {
+                        ModelState.AddModelError(string.Empty, "Вы не подтвердили свой email");
+                        return View(model);
+                    }
+                }
                 var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
@@ -227,10 +234,14 @@ namespace Garden.Controllers
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
-                    await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
+                    var callbackUrl = Url.Action(
+                                    "ConfirmEmail",
+                                    "Account",
+                                    new { userId = user.Id, code = code },
+                                    protocol: HttpContext.Request.Scheme);
+                    await _emailSender.SendEmailAsync(model.Email, "Подтверждение регистрации",
+                        $"Подтвердите регистрацию, перейдя по <a href='{callbackUrl}'>ссылке</a>:");
 
-                    await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation("User created a new account with password.");
                     return RedirectToLocal(returnUrl);
                 }
--- Garden/Services/EmailSender.cs
+++ Garden/Services/EmailSender.cs
@@ -1,16 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using MimeKit;
+using MailKit.Net.Smtp;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace Garden.Services
 {
-    // This class is used by the application to send email for account confirmation and password reset.
-    // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
     public class EmailSender : IEmailSender
     {
         public Task SendEmailAsync(string email, string subject, string message)
         {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Администрация сайта", "login@yandex.ru"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("Services/smtpsettings.json").Build();
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect(configuration["host"], Int32.Parse(configuration["port"]), false);
+                client.Authenticate(configuration["e-mail"], configuration["password"]);
+                client.Send(emailMessage);
+
+                client.Disconnect(true);
+            }
             return Task.CompletedTask;
         }
     }
